b1a3d0f5d61c36b78fa66d1d30c4b2f883f83be9
IceliusRegis <iceliusregis888@gmail.com>
2026-04-11 00:16:14 +0800

    Fixed map

---
+++ assets/environments/walls_roof.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.0" name="walls_roof" tilewidth="32" tileheight="32" tilecount="768" columns="32">
+ <image source="walls_roof.png" width="1024" height="768"/>
+</tileset>
